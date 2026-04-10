88cd8cc491a5a7db85453adbc9dca85e734fc22e
Niranjan Govinthan <your@email.com>
2026-04-10 16:46:05 +0530

    development deploy

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import hero from '../assets/hero.png'
-import logoOutline from '../assets/logo-outline.svg'
-import dotcircles from '../assets/dotcircles.svg'
 import { Terminal, github, linkedIn, mail, DevTo } from "./index"
 import TimeLine from './timeLine'
 import FloatingTags from './floatingTags'
@@ -27,10 +24,10 @@ function MainContainer() {
             <section id="home" className='w-full'>
                 {/* Social media bar */}
                 {width >= 768 ? (<div className='socialmedia__bar ml-[1.3rem] fixed top-[40%] flex md:flex-col flex-col gap-6 w-[32px]'>
-                    <a href='https://github.com/Niranjan-webdev' target='_blank'><img src={github} /></a>
-                    <a href='https://www.linkedin.com/in/niranjan-g2k/' target='_blank'><img src={linkedIn} /></a>
-                    {/* <a><img src={DevTo} /></a> */}
-                    <a href="mailto:niranjan.g2k@gmail.com" target="_blank"><img src={mail} /></a>
+                    <a href='https://github.com/Niranjan-webdev' target='_blank' rel="noopener noreferrer"><img src={github} alt="GitHub" /></a>
+                    <a href='https://www.linkedin.com/in/niranjan-g2k/' target='_blank' rel="noopener noreferrer"><img src={linkedIn} alt="LinkedIn" /></a>
+                   
+                    <a href="mailto:niranjan.g2k@gmail.com" target="_blank" rel="noopener noreferrer"><img src={mail} alt="Mail" /></a>
                 </div>) : ""}
 
                 {/* Social media bar */}
@@ -61,7 +58,7 @@ function MainContainer() {
                         <h4 className='text-lg md:text-xl font-bold text-[#2eb89c] mb-6'>— ABOUT ME</h4>
                         <h2 className='text-4xl md:text-5xl text-[#2eb89c] mb-6 font-bold'><span className='text-black'>I'm</span> Niranjan</h2>
                         <p className='text-base md:text-xl font-[600] mb-12'>Fullstack Developer with 4 years of experience specializing in React, TypeScript, and Node.js, with a strong focus on building scalable, user-focused web applications. Experienced in developing robust frontends and efficient backend APIs that simplify complex data operations. Proficient in creating interactive applications, reusable UI components, and performance-optimized applications using modern technologies such as React, Svelte, and Tailwind CSS. Strong problem-solving skills with a commitment to writing clean, maintainable code and collaborating effectively with cross-functional teams to deliver impactful digital products, while continuously exploring new technologies to enhance development practices.</p>
-                        <button className='flex gap-2 items-center py-4 px-16 bg-[#2eb89c] hover:bg-[#79cdb9] rounded-[12px] text-white text-base font-bold'>RESUME <RiDownload2Line /></button>
+                       <a href='/Niranjan_Frontend_Developer.pdf' download={"Niranjan_Frontend_Developer.pdf"}> <button className='flex gap-2 items-center py-4 px-16 bg-[#2eb89c] hover:bg-[#79cdb9] rounded-[12px] text-white text-base font-bold'>RESUME <RiDownload2Line /></button></a>
                     </div>
                 </div>
             </section>
